9f6e52be0d937ffae1aea89e18a580f3743412e3
tberey <tomberey1@gmail.com>
2024-10-17 23:55:45 +0100

    1.1.0

--- src/services/Helper.ts
+++ src/services/Helper.ts
@@ -39,4 +39,34 @@ export class Helper {
 
         return monthNames[monthNumber - 1];
     };
+
+    static parseDate(date: string | Date): number | null {
+        if (date) return new Date(date).getTime();
+        return null
+    }
+
+    static formatDate(dateString: string): string {
+        const date = new Date(dateString);
+
+        // Array of days and months
+        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+        // Get day, month, and year
+        const dayName = days[date.getUTCDay()];
+        const dayNumber = date.getUTCDate();
+        const monthName = months[date.getUTCMonth()];
+        const year = date.getUTCFullYear();
+        const suffixes = ['th', 'st', 'nd', 'rd'];
+
+        // Add ordinal suffix
+        const ordinalSuffix = (n: number): string => {
+            const value = n % 100;
+            // Ensure the index is valid
+            const suffixIndex = (value >= 10 && value <= 20) ? 0 : (value % 10);
+            return n + (suffixes[suffixIndex] || suffixes[0]!);
+        };
+
+        return `${dayName} ${ordinalSuffix(dayNumber)} ${monthName} ${year}`;
+    }
 }
